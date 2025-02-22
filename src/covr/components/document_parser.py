dab8de9b04e315d04c8f5de7a4b71a94831236d8
Aditya Banerjee <aditya_banerjee@icloud.com>
2025-02-22 11:17:20 +0000

    move parsing to separate component

---
+++ src/covr/components/document_parser.py
@@ -0,0 +1,18 @@
+from langchain_community.document_loaders import PyPDFLoader, PyPDFDirectoryLoader
+
+from covr.components.utils.file_utils import is_directory, is_pdf
+
+def parse(file):
+    if is_directory(file):
+        print(f"Processing directory: {file}")
+
+        loader = PyPDFDirectoryLoader(file)
+    elif not is_pdf(file):
+        raise ValueError("File must be a PDF")
+    else:
+        print(f"Processing file: {file}")
+
+        loader = PyPDFLoader(file)
+    
+    documents = loader.load()
+    return documents
--- src/covr/components/handlers/upload.py
+++ src/covr/components/handlers/upload.py
@@ -1,18 +1,6 @@
-from langchain_community.document_loaders import PyPDFLoader, PyPDFDirectoryLoader
-
-from covr.components.utils.file_utils import is_directory, is_pdf
+from covr.components.document_parser import parse
 
 def upload_handler(file):
-    if is_directory(file):
-        print(f"Processing directory: {file}")
+    documents = parse(file)
 
-        loader = PyPDFDirectoryLoader(file)
-    elif not is_pdf(file):
-        raise ValueError("File must be a PDF")
-    else:
-        print(f"Processing file: {file}")
-
-        loader = PyPDFLoader(file)
-    
-    documents = loader.load()
     print(documents[0])
