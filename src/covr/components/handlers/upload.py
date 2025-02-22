492c655bcf058c60225c466da748f5054b3b9af7
Aditya Banerjee <aditya_banerjee@icloud.com>
2025-02-22 11:09:13 +0000

    add single document handler

--- src/covr/components/handlers/upload.py
+++ src/covr/components/handlers/upload.py
@@ -1,14 +1,18 @@
-from langchain.document_loaders.pdf import PyPDFDirectoryLoader
+from langchain_community.document_loaders import PyPDFLoader, PyPDFDirectoryLoader
 
-from covr.components.utils.file_utils import is_directory
+from covr.components.utils.file_utils import is_directory, is_pdf
 
-def upload_handler(dir):
-    if is_directory(dir):
-        print(f"Processing directory: {dir}")
+def upload_handler(file):
+    if is_directory(file):
+        print(f"Processing directory: {file}")
 
-        loader = PyPDFDirectoryLoader(dir)
-        documents = loader.load()
-
-        print(documents[0])
+        loader = PyPDFDirectoryLoader(file)
+    elif not is_pdf(file):
+        raise ValueError("File must be a PDF")
     else:
-        raise ValueError("Uploaded file is not a directory")
+        print(f"Processing file: {file}")
+
+        loader = PyPDFLoader(file)
+    
+    documents = loader.load()
+    print(documents[0])
--- src/covr/components/utils/file_utils.py
+++ src/covr/components/utils/file_utils.py
@@ -4,3 +4,8 @@ def is_directory(path: str) -> bool:
     """Check if a path is a directory"""
 
     return os.path.isdir(path)
+
+def is_pdf(path: str) -> bool:
+    """Check if a path is a PDF file"""
+
+    return path.endswith(".pdf")
