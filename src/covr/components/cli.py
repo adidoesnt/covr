58f16478772b8b671148c6b021151209d5da9cfb
Aditya Banerjee <aditya_banerjee@icloud.com>
2025-02-22 11:11:37 +0000

    add alias for path option

--- src/covr/components/cli.py
+++ src/covr/components/cli.py
@@ -8,10 +8,10 @@ def cli():
     pass
 
 @click.command(name="upload")
-@click.option("--dir", prompt="Directory from which to upload files", help="Enter path to directory from which to upload files")
-def upload_files(dir):
+@click.option("--path", "-p", prompt="Directory or file to upload", help="Enter path to the directory or file to upload.")
+def upload_files(path):
     """Upload files to be stored in the knowledge base"""
 
-    upload_handler(dir)
+    upload_handler(path)
 
 cli.add_command(upload_files)
